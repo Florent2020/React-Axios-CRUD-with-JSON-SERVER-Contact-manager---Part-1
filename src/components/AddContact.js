3a8fca084e66e6fd58454892e389a20fac5d968f
Florent2020 <60365705+Florent2020@users.noreply.github.com>
2022-01-10 22:41:53 +0100

    first commit

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,55 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContactsCrud } from "../context/ContactsCrudContext";
 
-// function AddContact() {
-//   return <div>Add Contact</div>;
-// }
+const AddContact = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
 
-class AddContact extends React.Component {
-  state = {
-    name: "",
-    email: "",
-  };
+  const { addContactHandler } = useContactsCrud();
+  const navigate = useNavigate();
 
-  add = (e) => {
+  const add = (e) => {
+    console.log(e);
     e.preventDefault();
-    if (this.state.name === "" || this.state.email === "") {
-      alert("All the fields are mandatory");
+    if (name === "" || email === "") {
+      alert("ALl the fields are mandatory!");
       return;
     }
-    this.props.addContactHandler(this.state);
-    this.setState({ name: "", email: "" });
+    addContactHandler({ name, email });
+    setEmail("");
+    setName("");
+    navigate("/");
   };
-
-  render() {
-    return (
-      <div className="ui main">
-        <h2>Add Contact</h2>
-        <form className="ui form" onSubmit={this.add}>
-          <div className="field">
-            <label>Name</label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              value={this.state.name}
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
-          </div>
-          <div className="field">
-            <label>Email</label>
-            <input
-              type="text"
-              name="email"
-              placeholder="Email"
-              value={this.state.email}
-              onChange={(e) => this.setState({ email: e.target.value })}
-            />
-          </div>
-          <button className="ui button blue">Add</button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="ui main">
+      <h2>Add Contact</h2>
+      <form className="ui form" onSubmit={add}>
+        <div className="field">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="field">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button className="ui button blue">Add</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddContact;
--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+import ContactDetail from "./ContactDetail";
+import EditContact from "./EditContact";
+import { ContactsCrudContextProvider } from "../context/ContactsCrudContext";
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
-
-  const addContactHandler = (contact) => {
-    console.log(contact);
-    setContacts([...contacts, { id: uuidv4(), ...contact }]);
-  };
-
-  const removeContactHandler = (id) => {
-    const newContactList = contacts.filter((contact) => {
-      return contact.id !== id;
-    });
-
-    setContacts(newContactList);
-  };
-
-  useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retriveContacts) setContacts(retriveContacts);
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
     <div className="ui container">
       <Router>
         <Header />
-        <Routes>
-          <Route
-            path="/"
-            exact
-            // render={(props) => (
-            //   <ContactList
-            //     {...props}
-            //     contacts={contacts}
-            //     getContactId={removeContactHandler}
-            //   />
-            // )}
-            element={
-              <ContactList
-                contacts={contacts}
-                getContactId={removeContactHandler}
-              />
-            }
-          />
-          <Route
-            path="/add"
-            element={<AddContact addContactHandler={addContactHandler} />}
-          />
-        </Routes>
+        <ContactsCrudContextProvider>
+          <Routes>
+            <Route path="/" exact element={<ContactList />} />
+            <Route path="/add" element={<AddContact />} />
+
+            <Route path="/edit" element={<EditContact />} />
+
+            <Route path="/contact/:id" element={<ContactDetail />} />
+          </Routes>
+        </ContactsCrudContextProvider>
       </Router>
-      {/* <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
     </div>
   );
 }
